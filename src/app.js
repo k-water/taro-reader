ae46ee46f2f7642bbd6c97f3f89d8f69ac6688ba
k-water <625592890@qq.com>
2018-12-23 11:18:34 +0800

    feat: add tarbar config

--- src/app.js
+++ src/app.js
@@ -7,14 +7,53 @@ class App extends Component {
 
   config = {
     pages: [
-      'pages/index/index'
+      'pages/index/index',
+      'pages/class/index',
+      'pages/book/index',
+      'pages/person/index'
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
-    }
+    },
+    tabBar: {
+      color: '#000000',
+      borderStyle: 'white',
+      selectedColor: '#1296db',
+      list: [
+        {
+          pagePath: 'pages/index/index',
+          text: '发现',
+          iconPath: './static/icon/find.png',
+          selectedIconPath: './static/icon/find-selected.png'
+        },
+        {
+          pagePath: 'pages/class/index',
+          text: '分类',
+          iconPath: './static/icon/class.png',
+          selectedIconPath: './static/icon/class-selected.png'
+        },
+        {
+          pagePath: 'pages/book/index',
+          text: '书架',
+          iconPath: './static/icon/book.png',
+          selectedIconPath: './static/icon/book-selected.png'
+        },
+        {
+          pagePath: 'pages/person/index',
+          text: '我',
+          iconPath: './static/icon/person.png',
+          selectedIconPath: './static/icon/person-selected.png'
+        },
+      ]
+    },
+    networkTimeout: {
+      request: 10000,
+      downloadFile: 10000
+    },
+    debug: true
   }
 
   componentDidMount () {}
