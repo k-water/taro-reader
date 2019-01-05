70ce0a6d8d3342736cc9722426fae9fbe251eae1
k-water <625592890@qq.com>
2019-01-05 18:18:28 +0800

    feat: add classify tab

--- src/pages/class/index.js
+++ src/pages/class/index.js
@@ -3,6 +3,9 @@ import {
   View,
   Image
 } from '@tarojs/components'
+import {
+  AtTabBar
+} from 'taro-ui'
 import request from '../../utils'
 import './index.scss'
 
@@ -14,8 +17,11 @@ export default class Index extends Component {
   constructor () {
     super(...arguments)
     this.state = {
+      current: 0,
       statistics: []
     }
+
+    this.changeCurrent = this.changeCurrent.bind(this)
   }
 
   componentDidMount () {
@@ -34,29 +40,52 @@ export default class Index extends Component {
     })
     .then(res => {
       this.setState({
-        statistics: res.male
+        statistics: res
       });
     })
     .catch(err => {
       throw(err)
     })
   }
-  enterLv2 (tag) {
+  enterLv2 (tag, type) {
     Taro.navigateTo({
-      url: `detail/detail?tag=${tag}`
+      url: `/pages/classDetails/detail/detail?tag=${tag}&type=${type}`
+    })
+  }
+  changeCurrent (value) {
+    this.setState({
+      current: value
     })
   }
   render () {
     const ImageBaseUrl = 'http://statics.zhuishushenqi.com'
-    const { statistics } = this.state
+    const { statistics, current } = this.state
+    const typeList = ['male', 'female', 'press']
+    let list = statistics[typeList[current]]
     return (
       <View className='book-statistic'>
+        <AtTabBar
+          tabList={[
+            {
+              title: '男生',
+            },
+            {
+              title: '女生'
+            },
+            {
+              title: '出版物'
+            }
+          ]}
+          color='rgb(151, 154, 156)'
+          current={current}
+          onClick={this.changeCurrent}
+        />
         <View className='statistic-wrap'>
           {
-            statistics && statistics.map(item => (
+            list && list.map(item => (
               <View className='statistic-detail'
                 key={item.name}
-                onClick={this.enterLv2.bind(this, item.name)}
+                onClick={this.enterLv2.bind(this, item.name, typeList[current])}
               >
                 <View className='statistic-name'>
                   {item.name}
