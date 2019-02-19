b57b9e03d720210cb10bc0f6626c59a86a0f6ba6
k-water <625592890@qq.com>
2019-02-19 22:42:26 +0800

    feat: add bottom sheet

--- src/subpages/book/read/index.js
+++ src/subpages/book/read/index.js
@@ -21,7 +21,10 @@ export default class BookRead extends Component {
       currentIndex: 0,
       showDirectory: false,
       windowHeight: 0,
-      scrollCount: 2
+      scrollCount: 2,
+      showBottomSheet: false,
+      modeIndex: 0,
+      fontIndex: 2
     }
   }
 
@@ -71,16 +74,16 @@ export default class BookRead extends Component {
   }
 
   getChapterContent(index) {
+    const { bookAllDirectory } = this.state
+    if (index < 0 || index > bookAllDirectory.length) {
+      return
+    }
     this.setState({
       isLoading: true
     })
     Taro.showLoading({
       title: '加载中...'
     })
-    const { bookAllDirectory } = this.state
-    if (index < 0 || index > bookAllDirectory.length) {
-      return
-    }
     request({
       url: '/rapi/bookChapter',
       data: {
@@ -91,6 +94,7 @@ export default class BookRead extends Component {
         this.setState({
           chapterContent: res.chapter,
           currentIndex: index,
+          showBottomSheet: false,
           isLoading: false
         })
       })
@@ -101,13 +105,18 @@ export default class BookRead extends Component {
   }
 
   toggleDirectoryDrawer = () => {
-    const { showDirectory, bookAllDirectory } = this.state
+    const { showDirectory, showBottomSheet, bookAllDirectory } = this.state
     if (showDirectory) {
       this.setState({
         bookDirectory: bookAllDirectory.slice(0, 20),
         scrollCount: 2
       })
     }
+
+    if (showBottomSheet) {
+      this.toggleBottomSheet()
+    }
+
     this.setState({
       showDirectory: !showDirectory
     })
@@ -148,38 +157,144 @@ export default class BookRead extends Component {
     }
   }
 
+  toggleBottomSheet = () => {
+    const { showBottomSheet } = this.state
+    this.setState({
+      showBottomSheet: !showBottomSheet
+    })
+  }
+
+  changeMode(index, color) {
+    this.setState({
+      modeIndex: index
+    })
+    Taro.setNavigationBarColor({
+      frontColor: index === 1 ? '#ffffff' : '#000000',
+      backgroundColor: color
+    })
+    Taro.setBackgroundColor({
+      backgroundColor: color
+    })
+  }
+
+  changeFont(index) {
+    if (index < 0 || index > 5) {
+      return
+    }
+    this.setState({
+      fontIndex: index
+    })
+  }
+
   render() {
-    const { chapterContent, isLoading, currentIndex, showDirectory, bookDirectory, windowHeight } = this.state
+    const { chapterContent, isLoading, currentIndex, showDirectory, bookDirectory, windowHeight, showBottomSheet, bookAllDirectory, modeIndex, fontIndex } = this.state
     if (!isLoading) {
       return (
-        <View class='read-container'>
-          <View className='read-title'>
-            {chapterContent.title}
-          </View>
-          <View className='read-content'>
-            <Text>
-              {chapterContent.cpContent}
-            </Text>
-          </View>
-          <View className='read-action'>
-            <View
-              className='next-chapter'
-              onClick={this.getChapterContent.bind(this, currentIndex + 1)}
+        <View
+          class='read-container'
+          onLongPress={this.toggleBottomSheet}
+          style={
+            modeIndex === 0
+            ? 'background-color: #f2eeeb'
+            : (modeIndex === 1 ? 'background-color: #191919' : 'background-color: #c8edcc')
+          }
+        >
+          <View style={{padding: '20PX 10PX'}}>
+            <View className='read-title'
+              style={modeIndex === 1
+                ? `color: rgb(104, 104, 104); font-size: ${14 + fontIndex * 2}PX`
+                : `font-size: ${14 + fontIndex * 2}PX`
+              }
             >
-              <Text>下一章</Text>
+              {chapterContent.title}
             </View>
-            <View className='action-last'>
-              <View
-                className={currentIndex === 0 ? 'prev-chapter disabled' : 'prev-chapter'}
-                onClick={this.getChapterContent.bind(this, currentIndex - 1)}
+            <View className='read-content'>
+              <Text
+                style={modeIndex === 1
+                  ? `color: rgb(100, 100, 100); font-size: ${12 + fontIndex * 2}PX`
+                  : `font-size: ${12 + fontIndex * 2}PX`
+                }
               >
-                <Text>上一章</Text>
+                {chapterContent.cpContent}
+              </Text>
+            </View>
+            <View className='read-action'>
+              <View
+                className='next-chapter'
+                onClick={this.getChapterContent.bind(this, currentIndex + 1)}
+              >
+                <Text>下一章</Text>
+              </View>
+              <View className='action-last'>
+                <View
+                  className={currentIndex === 0 ? 'prev-chapter disabled' : 'prev-chapter'}
+                  onClick={this.getChapterContent.bind(this, currentIndex - 1)}
+                >
+                  <Text>上一章</Text>
+                </View>
+                <View className='read-director' onClick={this.toggleDirectoryDrawer}>
+                  <Text>目录</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View
+            className='bottom-sheet'
+            style={showBottomSheet
+              ? 'display: block; height: 190PX;'
+              : 'height: 0'
+            }
+          >
+            <View className='read-font'>
+              <View className='font-decrease' onClick={this.changeFont.bind(this, fontIndex - 1)}>
+                <Text>Aa-</Text>
+              </View>
+              <View className='font-line'>
+                <View
+                  className='progress-line'
+                  style={{width: `${fontIndex * 20}%`}}
+                />
+              </View>
+              <View className='font-increase' onClick={this.changeFont.bind(this, fontIndex + 1)}>
+                <Text>Aa+</Text>
+              </View>
+            </View>
+            <View className='read-mode'>
+              <View
+                className={modeIndex === 0 ? 'mode-default mode-actived' : 'mode-default'}
+                onClick={this.changeMode.bind(this, 0, '#f2eeeb')}
+              >
+                <Text>默认</Text>
               </View>
               <View
-                className='read-director'
-                onClick={this.toggleDirectoryDrawer}
+                className={modeIndex === 1 ? 'mode-night mode-actived' : 'mode-night'}
+                onClick={this.changeMode.bind(this, 1, '#191919')}
               >
+                <Text>夜间</Text>
+              </View>
+              <View
+                className={modeIndex === 2 ? 'mode-eye mode-actived' : 'mode-eye'}
+                onClick={this.changeMode.bind(this, 2, '#c8edcc')}
+              >
+                <Text>护眼</Text>
+              </View>
+            </View>
+            <View className='read-operate'>
+              <View
+                className='prev-chapter'
+                onClick={this.getChapterContent.bind(this, currentIndex - 1)}
+              >
+                <Text style={currentIndex === 0 ? 'color: rgb(124, 124, 124)' : ''}>上一章</Text>
+              </View>
+              <View className='book-director' onClick={this.toggleDirectoryDrawer}>
                 <Text>目录</Text>
+              </View>
+              <View
+                className='next-chapter'
+                onClick={this.getChapterContent.bind(this, currentIndex + 1)}
+              >
+                <Text style={currentIndex === bookAllDirectory.length ? 'color: rgb(124, 124, 124)' : ''}>下一章</Text>
               </View>
             </View>
           </View>
