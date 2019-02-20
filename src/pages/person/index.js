c4e15d2cd32775367b059f9409ee7d970dcab809
k-water <625592890@qq.com>
2019-02-20 22:48:09 +0800

    feat: add personal page

--- src/pages/person/index.js
+++ src/pages/person/index.js
@@ -1,21 +1,26 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
 // import { request } from '../../utils'
 import './index.scss'
 
 export default class Index extends Component {
   config = {
-    navigationBarTitleText: '个人中心'
+    navigationBarTitleText: '个人中心',
+    backgroundColor: '#e4e8eb'
   }
 
   constructor() {
     super(...arguments)
+    this.setState({
+      userInfo: {}
+    })
   }
 
-  componentDidMount() {
-  }
   componentDidShow() {
     this.checkAuth()
+  }
+  componentDidMount() {
+    this.getUserInfo()
   }
   checkAuth() {
     Taro.getSetting({
@@ -29,10 +34,32 @@ export default class Index extends Component {
     })
   }
 
+  getUserInfo() {
+    const self = this
+    Taro.getUserInfo({
+      success(res) {
+        self.setState({
+          userInfo: res.userInfo
+        })
+      }
+    })
+  }
+
   render() {
+    const { userInfo } = this.state
     return (
-      <View>
-        <Button>test</Button>
+      <View className='personal-container'>
+        <View className='personal-info'>
+          <View className='personal-name'>
+            <Text>{userInfo.nickName}</Text>
+          </View>
+          <View className='personal-avatar'>
+            <Image
+              mode='aspectFill'
+              src={userInfo.avatarUrl}
+            />
+          </View>
+        </View>
       </View>
     )
   }
