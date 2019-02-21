63494f0cd46c0b7a5915860eb15aff8df323a910
k-water <625592890@qq.com>
2019-02-21 17:56:39 +0800

    feat: add cloud func delBook

---
+++ src/cloudFunctions/delBook/index.js
@@ -0,0 +1,23 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event) => new Promise((resolve, reject) => {
+ try {
+   db.collection('books').where({
+     bookId: event.bookId
+   })
+    .remove()
+    .then(res => {
+      resolve(res)
+    })
+    .catch(err => {
+      reject(err)
+    })
+ } catch(e) {
+   reject(e)
+ }
+})
